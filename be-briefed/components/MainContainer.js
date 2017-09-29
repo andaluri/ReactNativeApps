04d650500306741cfd5bac7ddf806b7fb240e4c7
andaluri <andaluri@gmail.com>
2017-09-28 21:03:50 -0400

    Vector icons
    Work in progress

---
+++ be-briefed/components/MainContainer.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import {
+	View,
+	Text,
+	StyleSheet,
+} from 'react-native'
+
+import StatusScreen from './StatusScreen'
+
+class MainContainer extends Component {
+	render() {
+		return (
+			<View style={styles.container}>
+				<StatusScreen />
+			</View>
+		)
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	}
+})
+
+export default MainContainer
---
+++ be-briefed/components/StatusScreen.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import {
+	View,
+	Text,
+	StyleSheet,
+} from 'react-native'
+
+import StatusIndicator from './StatusIndicator'
+
+class StatusScreen extends Component {
+	render() {
+		return (
+			<View style={styles.container}>
+				<StatusIndicator />
+				<Text style={styles.statusText}>Service Up</Text>
+			</View>
+		)
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+
+	statusText: {
+		fontSize: 30,
+	},
+
+})
+
+export default StatusScreen
+
